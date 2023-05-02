24f14692c33145f96b5a6290b479187227803ef6
johnbroaddusivccv <johnbroaddusivccv@gmail.com>
2023-05-02 11:52:59 -0400

    hello world function rust

---
+++ hello_world/target/debug/deps/hello_world-1bf1d761e8c07943.d
@@ -0,0 +1,5 @@
+/Users/john/Desktop/rust/rust_curriculum/hello_world/target/debug/deps/hello_world-1bf1d761e8c07943.rmeta: src/main.rs
+
+/Users/john/Desktop/rust/rust_curriculum/hello_world/target/debug/deps/hello_world-1bf1d761e8c07943.d: src/main.rs
+
+src/main.rs:
---
+++ hello_world/target/debug/deps/hello_world-605dd603550c28b7.d
@@ -0,0 +1,5 @@
+/Users/john/Desktop/rust/rust_curriculum/hello_world/target/debug/deps/hello_world-605dd603550c28b7.rmeta: src/main.rs
+
+/Users/john/Desktop/rust/rust_curriculum/hello_world/target/debug/deps/hello_world-605dd603550c28b7.d: src/main.rs
+
+src/main.rs:
